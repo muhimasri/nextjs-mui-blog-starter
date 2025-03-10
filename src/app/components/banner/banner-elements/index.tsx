3973df0d584ee0a62f989270cc8ae14b8a56d40c
Muhi Masri <muhi@muhimasri.com>
2025-03-10 19:41:10 -0400

    Add banner folder

---
+++ src/app/components/banner/banner-elements/index.tsx
@@ -0,0 +1,73 @@
+"use client";
+import type {} from "@mui/material/themeCssVarsAugmentation";
+import { Stack, styled, Typography } from "@mui/material";
+import Image from "next/image";
+import { ReactNode } from "react";
+
+const BannerImage = styled(Image)(({ theme }) => ({
+  borderRadius: theme.shape.radius.large,
+  boxShadow: theme.shadows[2],
+  maxWidth: "100%",
+  maxHeight: "fit-content",
+}));
+
+function ImageFrame(): JSX.Element {
+  return (
+    <BannerImage
+      alt="banner-image"
+      src="/images/banner-image.jpg"
+      width={500}
+      height={320}
+    />
+  );
+}
+
+function TagLine(): JSX.Element {
+  return (
+    <Typography
+      component="span"
+      sx={{
+        fontSize: { xs: "8", sm: "9" },
+        color: "text.primary",
+        position: "relative",
+        fontWeight: 700,
+        lineHeight: (theme) => theme.typography.lineHeight.small,
+      }}
+    >
+      Innovating the Future of{" "}
+      <Typography
+        fontSize="inherit"
+        fontWeight="inherit"
+        component="span"
+        sx={{ color: "primary.main" }}
+      >
+        Software Development
+      </Typography>
+    </Typography>
+  );
+}
+
+function BannerContainer({
+  children,
+  sx,
+}: {
+  children: ReactNode;
+  sx?: object;
+}) {
+  return (
+    <Stack
+      flexDirection="column"
+      justifyContent="center"
+      sx={{
+        pt: { xs: 0, lg: 6 },
+        pb: 8,
+        background: (theme) => theme.vars.palette.banner.background,
+        ...sx,
+      }}
+    >
+      {children}
+    </Stack>
+  );
+}
+
+export { ImageFrame, TagLine, BannerContainer };
